b207b5a11b530ac3b951f38d4cd03d5569db9eb6
Saiful Aziz <gamestopik712@gmail.com>
2026-02-28 23:29:58 +0500

    Add Room  entity

---
+++ src/main/java/com/SaifiDev/HotelManagementSystem/entity/Room.java
@@ -0,0 +1,39 @@
+package com.SaifiDev.HotelManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+import org.hibernate.boot.models.xml.internal.attr.BasicIdAttributeProcessing;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+    private List<Booking> bookings = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", roomType='" + roomType + '\'' +
+                ", roomPrice=" + roomPrice +
+                ", roomPhotoUrl='" + roomPhotoUrl + '\'' +
+                ", roomDescription='" + roomDescription + '\'' +
+                '}';
+    }
+}
+
+
