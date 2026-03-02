0639c65a5d294ae5164bee31a8634efa82cd3ab1
Saiful Aziz <gamestopik712@gmail.com>
2026-03-03 01:57:08 +0500

    Add RoomRepository

---
+++ src/main/java/com/SaifiDev/HotelManagementSystem/repository/RoomRepository.java
@@ -0,0 +1,21 @@
+package com.SaifiDev.HotelManagementSystem.repository;
+
+import com.SaifiDev.HotelManagementSystem.entity.Room;
+import org.joda.time.LocalDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    @Query("SELECT DISTINCT r.roomType FROM Room r")
+    List<String> findDistinctRoomType();
+
+    @Query("SELECT r FROM Room r WHERE r.roomType LIKE %:roomType AND r.id NOT IN (SELECT bk.room.id FROM Booking bk WHERE " +
+            "(bk.checkInDate <= :checkOutDate) AND (bk.checkOutDate >= :checkInDate))")
+    List<Room> findAvailableRoomsByDatesAndTypes(LocalDate CheckInDate, LocalDate CheckOutDate, String roomType);
+
+    @Query("SELECT r FROM Room r WHERE r.id NOT IN (SELECT b.room.id FROM Booking b)")
+    List<Room> getAllAvailableRooms();
+}
