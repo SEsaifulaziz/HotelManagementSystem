2ac57e293bb6e4d3b6b19ccd176576dbd0f1a232
Saiful Aziz <gamestopik712@gmail.com>
2026-03-03 15:38:52 +0500

    Add CorsConfig

---
+++ src/main/java/com/SaifiDev/HotelManagementSystem/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.SaifiDev.HotelManagementSystem.security;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
